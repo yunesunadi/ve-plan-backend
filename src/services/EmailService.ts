df7288d1767ca80ff7a586f5c5f537b125019603
Yune Su Nadi <yunesunadi.dev@gmail.com>
2025-08-02 23:48:08 +0630

    changed sending email with nodemailer

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,61 +1,73 @@
-const sgMail = require("@sendgrid/mail");
+import * as nodemailer from "nodemailer";
+import * as fs from "fs";
+import * as path from "path";
 
 require("dotenv").config();
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export async function send(obj: { action: string; recipient: string; additional: any; }) {
+const transporter = nodemailer.createTransport({
+  service: process.env.SMTP_SERVICE,
+  auth: {
+    type: "OAuth2",
+    user: process.env.SMTP_USER,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
+
+const getEmailTemplate = (templateName: string, variables: any) => {
+  const templatePath = path.join(__dirname, "..", "email_templates", `${templateName}.html`);
+  
+  try {
+    let template = fs.readFileSync(templatePath, "utf8");
+    
+    Object.keys(variables).forEach(key => {
+      const regex = new RegExp(`{{${key}}}`, "g");
+      template = template.replace(regex, variables[key]);
+    });
+    
+    return template;
+  } catch (error) {
+    console.error(`Error reading template ${templateName}:`, error);
+  }
+}
+
+export const send = async (obj: { action: string; recipient: string; additional: any; }) => {
   const basic_info = {
     to: obj.recipient,
     from: process.env.SENDER,
   };
-  let msg: any;
+  
+  const templateName = obj.action;
 
-  if (obj.action === "email_verified") {
-    msg = {
-      ...basic_info,
-      templateId: `${process.env.EMAIL_VERIFIED_TEMPLATE}`,
-      dynamicTemplateData: {
-        name: obj.additional.name,
-        link: obj.additional.link,
-      },
-    };
-  } else if (obj.action === "reset_password") {
-    msg = {
-      ...basic_info,
-      templateId: `${process.env.RESET_PASSWORD_TEMPLATE}`,
-      dynamicTemplateData: {
-        name: obj.additional.name,
-        link: obj.additional.link,
-      },
-    };
-  } else if (obj.action === "register_approved") {
-    msg = {
-      ...basic_info,
-      templateId: `${process.env.REGISTER_APPROVAL_TEMPLATE}`,
-      dynamicTemplateData: {
-        name: obj.additional.name,
-        event_title: obj.additional.event_title,
-      },
-    };
-  } else if (obj.action === "invitation_sent") {
-    msg = {
-      ...basic_info,
-      templateId: `${process.env.INVITATION_SENT_TEMPLATE}`,
-      dynamicTemplateData: {
-        name: obj.additional.name,
-        event_title: obj.additional.event_title,
-      },
-    };
-  } else if (obj.action === "meeting_started") {
-    msg = {
-      ...basic_info,
-      templateId: `${process.env.MEETING_STARTED_TEMPLATE}`,
-      dynamicTemplateData: {
-        name: obj.additional.name,
-        event_title: obj.additional.event_title,
-      },
-    };
+  const variables = {
+    name: obj.additional.name,
+    event_title: obj.additional.event_title,
+    link: obj.additional.link,
+  };
+
+  const htmlContent = getEmailTemplate(templateName, variables);
+
+  const mailOptions = {
+    ...basic_info,
+    subject: getEmailSubject(obj.action),
+    html: htmlContent,
+  };
+
+  return transporter.sendMail(mailOptions);
+}
+
+const getEmailSubject = (action: string) => {
+  switch (action) {
+    case "email_verified":
+      return "Email Verification";
+    case "reset_password":
+      return "Password Reset";
+    case "register_approved":
+      return "Registration Approved";
+    case "invitation_sent":
+      return "Event Invitation";
+    case "meeting_started":
+      return "Meeting Started";
   }
-
-  return sgMail.send(msg);
 }
