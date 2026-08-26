570900f703a10cfcb875d45a5d380713c5775300
Yune Su Nadi <yunesunadi.dev@gmail.com>
2026-08-26 11:31:30 +0700

    updated config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       cwd: __dirname,
       instances: 1,
       exec_mode: "fork",
-      watch: true,
+      watch: false,
       autorestart: true,
       max_memory_restart: "500M",
       env: {
